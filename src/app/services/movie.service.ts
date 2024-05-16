5d788464ebe3cd62a26447ad646c3d091f80bc95
Marina <marina.guderian@gmail.com>
2024-05-16 16:23:34 -0400

    Crud Movie service

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Movie } from '../models/movie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  dataSource: string = "http://localhost:3000/movies"
+
+  constructor(private http: HttpClient) { }
+
+  getAllMovies(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(this.dataSource);
+  }
+
+  getMovieByID(id: number): Observable<Movie> {
+    return this.http.get<Movie>(this.dataSource + "/" + id);
+  }
+
+  createNewMovie(newMovie: Movie): Observable<Movie>{
+    return this.http.post<Movie>(this.dataSource, newMovie);
+  }
+
+  editMovieByID(id: number, edittedMovie: Movie): Observable<Movie> {
+    return this.http.put<Movie>(this.dataSource + "/" + id, edittedMovie);
+  }
+
+  deleteMovieByID(id: number): Observable<any> {
+    return this.http.delete<any>(this.dataSource + "/" + id)
+  }
 }
